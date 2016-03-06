f10abbebb57f35383440460c7fd65483d665cab7
Ali Taheri <ali.taheri.m@gmail.com>
2016-03-06 16:55:46 +0330

    Partially implement pipeline

--- src/pipeline.ts
+++ src/pipeline.ts
@@ -2,6 +2,33 @@ import {Context, StepResult} from './index';
 
 export interface Pipeline {
   addStep(step: string): this;
-  execute(): this;
-  accumulation: number[];
+  execute(): Promise<number[]>;
+}
+
+interface StepData {
+  dependencies: string[];
+  apply(context: Context): Promise<number>;
+}
+
+function getPreviousStepIds(currentStep: number): string[] {
+  const ids = [];
+  for (let i = 1; i < currentStep; i++) {
+    ids.push(`STEP_${i}`);
+  }
+  return ids;
+}
+
+function pipeline(context: Context): Pipeline {
+
+  const steps: StepData[] = [];
+  const params = Object.keys(context);
+
+  function addStep(step: string) {
+
+
+
+  }
+
+  return null;
+
 }
